439400bf3982f483250733540cd2d2aac83c3104
jhoh <jaewhan0995@naver.com>
2022-04-09 00:17:07 +0900

    [FEAT] add class classifier and state classifier

--- models/get_models.py
+++ models/get_models.py
@@ -1,4 +1,5 @@
 import timm
+import torch
 import torch.nn as nn
 
 
@@ -10,5 +11,32 @@ class Detector(nn.Module):
 
         self.model.reset_classifier(num_classes=88)
     
+    @torch.cuda.amp.autocast()
     def forward(self, x):
         return self.model(x)
+
+
+class ClassClassifier(nn.Module):
+    def __init__(self, num_features=1000, num_classes=15, drop=0.1):
+        super().__init__()
+        self.classifier = nn.Linear(num_features, num_classes)
+        self.act = nn.GELU()
+        self.drop = nn.Dropout(drop)
+
+    @torch.cuda.amp.autocast()
+    def forward(self, x):
+        return self.classifier(self.act(self.drop(x)))
+
+
+class StateClassifier(nn.Module):
+    def __init__(self, num_features=1000, num_classes=9, drop=0.1):
+        super().__init__()
+        self.classifier = nn.Linear(num_features, num_classes)
+        self.act = nn.GELU()
+        self.drop = nn.Dropout(drop)
+
+    @torch.cuda.amp.autocast()
+    def forward(self, x, mask):
+        x = self.classifier(self.act(self.drop(x)))
+        x.masked_fill_(mask, -10000.)
+        return x
